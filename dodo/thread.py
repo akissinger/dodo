6e7fb4401b15373c1b938d1823455d7c17559926
Aleks Kissinger <aleks0@gmail.com>
2021-07-28 17:40:32 +0100

    functions for finding content

--- dodo/thread.py
+++ dodo/thread.py
@@ -2,6 +2,25 @@ from PyQt5.QtWidgets import QWidget
 import subprocess
 import json
 
+# recursively search a message body for content of a particular type and return the first
+# occurance
+def find_content(m, ty):
+    content = []
+
+    def dfs(x):
+        global content, ty
+        if isinstance(x, list):
+            for y in x: dfs(y)
+        elif isinstance(x, dict) and 'content-type' in x and 'content' in x:
+            if x['content-type'] == ty:
+                content.append(x['content'])
+            elif isinstance(x['content'], list):
+                for y in x['content']: dfs(y)
+
+    dfs(m)
+    return content
+
+
 class ThreadView(QWidget):
     def __init__(self, app, thread_id, parent=None):
         super().__init__(parent)
@@ -10,7 +29,7 @@ class ThreadView(QWidget):
         self.refresh()
 
     def refresh(self):
-        r = subprocess.run(['notmuch', 'show', '--format=json', self.thread_id],
+        r = subprocess.run(['notmuch', 'show', '--format=json', '--include-html', self.thread_id],
                 stdout=subprocess.PIPE)
         self.json_str = r.stdout.decode('utf-8')
         self.d = json.loads(self.json_str)
@@ -21,8 +40,13 @@ class ThreadView(QWidget):
         self.thread.sort(key=lambda m: m['timestamp'])
         print('thread of size: %d' % len(self.thread))
 
+        for m in self.thread:
+            c = find_content(m['body'], 'text/html')
+            if not c: c = find_content(m['body'], 'text/plain')
+
     def dft(self, d):
         if isinstance(d, list):
             for x in d: self.dft(x)
         else:
             self.thread.append(d)
+
