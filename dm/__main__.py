87d7df96b6fc628317415fcecce36767928d931a
Aleks Kissinger <aleks0@gmail.com>
2021-07-28 10:30:35 +0100

    dodo app object

--- dm/__main__.py
+++ dm/__main__.py
@@ -1,13 +1,5 @@
-from PyQt5.QtWidgets import QApplication
-
-from . import search
-from . import style
+from . import app
 
 if __name__ == '__main__':
-    app = QApplication([])
-    app.setApplicationName('Dodo')
-    style.apply_style(app)
-    view = search.SearchView('tag:inbox')
-    view.resize(1024,768)
-    view.show()
-    app.exec()
+    dodo = app.Dodo()
+    dodo.exec()
---
+++ dm/app.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QApplication
+
+from . import search
+from . import style
+
+class Dodo(QApplication):
+    def __init__(self):
+        super().__init__([])
+        self.setApplicationName('Dodo')
+        style.apply_style(self)
+        self.inbox = search.SearchView('tag:inbox')
+        self.inbox.resize(1024, 768)
+        self.inbox.show()
+
