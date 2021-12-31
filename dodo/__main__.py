fd6d5c138b32b5fa20edf1077a0a01d40677ab25
Aleks Kissinger <aleks0@gmail.com>
2021-12-31 15:08:26 +0000

    GPL-ized

--- dodo/__main__.py
+++ dodo/__main__.py
@@ -1,3 +1,21 @@
+#     Dodo - A graphical, hackable email client based on notmuch
+#     Copyright (C) 2021 - Aleks Kissinger
+#
+# This file is part of Dodo
+#
+# Dodo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dodo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dodo. If not, see <https://www.gnu.org/licenses/>.
+
 from . import app
 
 if __name__ == '__main__':
