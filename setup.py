c9fb3af2b87838c8b0620b8342d9492c3087252f
Aleks Kissinger <aleks0@gmail.com>
2022-01-02 16:52:50 +0000

    project metadata

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="agentdodo",
+    version="0.1",
+    author="Aleks Kissinger",
+    author_email="aleks0@gmail.com",
+    description="A graphical, hackable email client based on notmuch ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akissinger/dodo",
+    project_urls={
+        "Bug Tracker": "https://github.com/akissinger/dodo/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=["dodo"],
+    install_requires=["PyQt5>=5.14", "PyQtWebEngine>=5.14"],
+    include_package_data=True,
+    python_requires=">=3.7",
+)
