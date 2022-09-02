63970484fb1b1a4a1b04edac0b24aea613f1d203
Aleks Kissinger <aleks0@gmail.com>
2022-09-02 11:24:21 +0100

    added catppuchin theme

--- dodo/themes.py
+++ dodo/themes.py
@@ -60,6 +60,62 @@ solarized_p = {
   'green':     '#859900',
 }
 
+# Catppuccin pallette
+# https://github.com/catppuccin/catppuccin
+cat_macchiato_p = {
+  'rosewater':  '#f4dbd6',
+  'flamingo':   '#f0c6c6',
+  'pink':       '#f5bde6',
+  'mauve':      '#c6a0f6',
+  'red':        '#ed8796',
+  'maroon':     '#ee99a0',
+  'peach':      '#f5a97f',
+  'yellow':     '#eed49f',
+  'green':      '#a6da95',
+  'teal':       '#8bd5ca',
+  'sky':        '#91d7e3',
+  'sapphire':   '#7dc4e4',
+  'blue':       '#8aadf4',
+  'lavender':   '#b7bdf8',
+  'text':       '#cad3f5',
+  'subtext1':   '#b8c0e0',
+  'subtext0':   '#a5adcb',
+  'overlay2':   '#939ab7',
+  'overlay1':   '#8087a2',
+  'overlay0':   '#6e738d',
+  'surface2':   '#5b6078',
+  'surface1':   '#494d64',
+  'surface0':   '#363a4f',
+  'base':       '#24273a',
+  'mantle':     '#1e2030',
+  'crust':      '#181926',
+}
+
+catppuccin_macchiato = {
+  'bg': cat_macchiato_p['base'],
+  'fg': cat_macchiato_p['text'],
+  'fg_bright': cat_macchiato_p['lavender'],
+  'fg_dim': cat_macchiato_p['subtext0'],
+  'fg_good': cat_macchiato_p['green'],
+  'fg_bad': cat_macchiato_p['red'],
+  'bg_alt': cat_macchiato_p['surface1'],
+  'bg_button': cat_macchiato_p['overlay1'],
+  'fg_button': cat_macchiato_p['crust'],
+  'fg_link': cat_macchiato_p['blue'],
+  'bg_highlight': cat_macchiato_p['peach'],
+  'fg_highlight': cat_macchiato_p['crust'],
+  'fg_subject': cat_macchiato_p['text'],
+  'fg_subject_unread': cat_macchiato_p['sapphire'],
+  'fg_subject_flagged': cat_macchiato_p['yellow'],
+  'fg_from': cat_macchiato_p['lavender'],
+  'fg_date': cat_macchiato_p['mauve'],
+  'fg_tags': cat_macchiato_p['teal'],
+}
+"""Theme based on the `Catppuchin`_ palette (macchiatto version).
+
+.. _Catppuchin: https://github.com/catppuccin/catppuccin
+"""
+
 solarized_dark = {
   'bg': solarized_p['base02'],
   'fg': solarized_p['base1'],
